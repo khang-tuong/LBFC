44d9f2df17a1ddef604fd809a4ace2852535f73e
khang-tuong <jakelong1232007@gmail.com>
2017-09-09 15:02:17 +0700

    Tạo project page admin

---
+++ LBFC/LBFC/Models/Services/BaseService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using LBFC.Models.Repositories;
+
+namespace LBFC.Models.Services
+{
+    public abstract class BaseService<E> where E : class
+    {
+        protected IRepository<E> repo;
+
+        public BaseService(E e)
+        {
+            this.repo = new Repository<E>();
+        }
+
+        public E GetById(int id) {
+            return this.repo.Get<int>(id);
+        }
+    }
+}
