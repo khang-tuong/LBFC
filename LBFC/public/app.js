ac38f6c1f66c67960a9becfa535ce44ce721a800
baothdse <baothdse@gmail.com>
2017-09-14 14:13:58 +0700

    Commit create Hero Card

--- LBFC/public/app.js
+++ LBFC/public/app.js
@@ -8,7 +8,7 @@ var morgan = require('morgan');
 var app = express();
 
 //morgan
-app.use(morgan('dev'));
+app.use(morgan('dev'));  
 
 //body-parser
 app.use(bodyParser.urlencoded({
@@ -41,12 +41,28 @@ bot.dialog('getProduct', [
         var coffeeShopEntity = botbuilder.EntityRecognizer.findEntity(intent.entities, 'Coffee Shop');
         var restaurant = botbuilder.EntityRecognizer.findEntity(intent.entities, 'Restaurant');
         var clothesShop = botbuilder.EntityRecognizer.findEntity(intent.entities, 'Clothes Shop');
-        var message = 'Bạn chờ xíu mình cho bạn xem ';
-
+        var message = new botbuilder.Message(session);
+        message.attachmentLayout(botbuilder.AttachmentLayout.carousel);
         if (coffeeShopEntity) {
             session.dialogData.searchType = 'Coffee Shop';
-            message += 'nước uống của ' + coffeeShopEntity.resolution.values;
-            
+            message.attachments([
+                new botbuilder.HeroCard(session)
+                    .title('Ba cô gái')
+                    .subtitle("Bò bít tết sốt rượu vang")
+                    .text("Size nhỏ: 45k, size lớn: 60k")
+                    .images([botbuilder.CardImage.create(session, 'http://vinbeefood.com/timthumb.php?src=upload/images/bo-bit-tet-va-bo-ne.png')])
+                    .buttons([
+                        botbuilder.CardAction.imBack(session, "Order", "Order")
+                    ]),
+                new botbuilder.HeroCard(session)
+                    .title('Gong Cha')
+                    .subtitle("Trà sữa Gong Cha")
+                    .text("Size nhỏ: 43k, size lớn: 55k")
+                    .images([botbuilder.CardImage.create(session, 'http://www.gongchausa.com/wp-content/uploads/2017/01/Coffee-Milk-Tea-722x1024.png')])
+                    .buttons([
+                        botbuilder.CardAction.imBack(session, "Order", "Order")
+                    ]),
+            ]);
         } else if (restaurant) {
             session.dialogData.searchType = 'Restaurant';
             message += 'món ăn của ' + restaurant.resolution.values;
@@ -54,15 +70,13 @@ bot.dialog('getProduct', [
             session.dialogData.searchType = 'Clothes Shop';
             message += 'sản phẩm của ' + clothesShop.resolution.values;
         } else {
-            message = "Xin lỗi, quán này không nằm trong chuỗi hệ thống của chúng tôi!";
+            message = "Xin lỗi, cửa hàng này không nằm trong chuỗi hệ thống của chúng tôi!";
         }
         session.send(message);
-        
     }
 ]).triggerAction({
     matches: ['GetCoffeeShopProduct', 'GetRestaurantProduct', 'GetClothesShopProduct'],
     confirmPrompt: 'Minh cho bạn xem menu của %s nhé?' 
 });
-
 
 console.log("Server start on port: " + port);
