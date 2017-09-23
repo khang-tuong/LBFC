c86935e62d0c5dd8013be3ba279858274f045cc1
baothdse <baothdse@gmail.com>
2017-09-23 11:24:18 +0700

    Create Hero Card

--- LBFC/public/bot/bot-config.js
+++ LBFC/public/bot/bot-config.js
@@ -11,36 +11,26 @@ var connector = new botbuilder.ChatConnector({
 var bot = new botbuilder.UniversalBot(connector, function (session) {
     session.send("Uni Space kính chào quý khách! Hôm này chúng tôi có: ");
     var message = new botbuilder.Message(session);
-    session.sendTyping();
     message.attachmentLayout(botbuilder.AttachmentLayout.carousel);
     message.attachments([
-        new botbuilder.HeroCard(session)
-            .title("Mocha")
-            .subtitle("Coffee, Chocolate, Vani, Đá")
-            .images([botbuilder.CardImage.create(session, 'https://i.pinimg.com/736x/94/d4/49/94d449968b99699f959e096bb2c812e0--homemade-chocolate-pudding-pudding-recipes.jpg')])
-            .buttons([
-                botbuilder.CardAction.imBack(session, "Order", "Order")
-            ]),
-        new botbuilder.HeroCard(session)
-            .title("Banana Cake")
-            .subtitle("Bánh chuối siêu hạng")
-            .images([botbuilder.CardImage.create(session, 'https://i.pinimg.com/736x/9b/8d/6b/9b8d6b453ad25b71fcf44a01a87f92e0--banana-upside-down-cake-upside-down-cakes.jpg')])
-            .buttons([
-                botbuilder.CardAction.imBack(session, "Order", "Order")
-            ]),
-        new botbuilder.HeroCard(session)
-            .title("Matcha Ice Blended")
-            .subtitle("Matcha, Cream, Ice")
-            .images([botbuilder.CardImage.create(session, 'http://coffeenowhere.com/order/wp-content/uploads/2016/07/matcha-latte.png')])
-            .buttons([
-                botbuilder.CardAction.imBack(session, "Order", "Order")
-            ])
+        createHeroCard(session, "Mocha", "Coffee, Chocolate, Vani, Đá",
+            'https://i.pinimg.com/736x/94/d4/49/94d449968b99699f959e096bb2c812e0--homemade-chocolate-pudding-pudding-recipes.jpg',
+            'Đặt hàng'),
+        createHeroCard(session, "Banana Cake", "Bánh chuối siêu hạng",
+            'https://i.pinimg.com/736x/9b/8d/6b/9b8d6b453ad25b71fcf44a01a87f92e0--banana-upside-down-cake-upside-down-cakes.jpg',
+            'Đặt hàng'),
+        createHeroCard(session, "Matcha Ice Blended", "Matcha, Cream, Ice",
+            'http://coffeenowhere.com/order/wp-content/uploads/2016/07/matcha-latte.png',
+            'Đặt hàng')
     ]);
+
+    var card = createYesNoCard(session);
     setTimeout(function () {
-        moreMessage = "Bạn có muốn xem thêm không";
-        session.send(moreMessage);
+        message = "Bạn có muốn xem thêm không";
+        session.send(message);
+        session.send(new botbuilder.Message(session).addAttachment(card));
     }, 5000);
-    session.send(message)
+    session.send(message);
 });
 //luis
 var luisAppUrl = process.env.LUIS_APP_URL || 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/f27af6e6-01da-4685-ac7a-1e5e7cd744d8?subscription-key=747a9082c49148218afb177584a54ec4&timezoneOffset=0&verbose=true&q=';
@@ -82,6 +72,28 @@ function beginDialog(address, dialogId, dialogArgs) {
 function sendMessage(message) {
     bot.send(message);
 };
+
+
+//Card
+function createHeroCard(session, title, subtitle, imageLink, buttonTitle) {
+    return new botbuilder.HeroCard(session)
+        .title(title)
+        .subtitle(subtitle)
+        .images([
+            botbuilder.CardImage.create(session, imageLink)
+        ])
+        .buttons([
+            botbuilder.CardAction.imBack(session, buttonTitle, buttonTitle)
+        ])
+}
+
+function createYesNoCard(session) {
+    return new botbuilder.HeroCard(session)
+        .buttons([
+            botbuilder.CardAction.imBack(session, "Có", "Có"),
+            botbuilder.CardAction.imBack(session, "Không", "Không")
+        ])
+}
 
 module.exports = {
     listen: listen,
--- LBFC/public/bot/dialogs/order.js
+++ LBFC/public/bot/dialogs/order.js
@@ -1,2 +1,8 @@
 var botbuilder = require('botbuilder');
 var lib = new botbuilder.Library('order');
+
+lib.dialog('orderProduct',[
+    function(session, args, next) {
+        var intent = args.intent;
+    }
+])
